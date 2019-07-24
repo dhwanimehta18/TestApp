d841a02c1ed6dba98ebbbe4dc4b0dc5f69e98dcd
dhwanimehta18 <33513769+dhwanimehta18@users.noreply.github.com>
2019-07-24 09:38:04 +0530

    Delete FCMNotificationResponse.kt

--- app/src/main/java/com/example/testapp/testapp/DatabaseExample/api/response/notification/FCMNotificationResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.testapp.testapp.DatabaseExample.api.response.notification
-
-class FCMNotificationResponse(
-    val body: String,
-    val type: Int,
-    val sound: String,
-    val title: String,
-    val message: String
-)
